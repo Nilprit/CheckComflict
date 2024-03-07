f26ff83f7f3aaf545ffd4a2d187043927b6d4ad8
Nilprit <161545011+Nilprit@users.noreply.github.com>
2024-03-07 19:54:20 +0530

    add project

--- src/test/java/Git_poject/Program.java
+++ src/test/java/Git_poject/Program.java
@@ -2,11 +2,11 @@ package Git_poject;
 
 public class Program {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
-		System.out.println("Prgram Files");
-		
-		
+		System.out.println("Prgram Files for conflict");
+				
 	}
 
 }
